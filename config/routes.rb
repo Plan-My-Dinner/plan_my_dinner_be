e4a4a72c60edab309dad5f5e8bc214a877aebb0d
Tamara Dowis <tamara.dowis@gmail.com>
2023-07-21 17:12:37 -0600

    create  GET - Random Recipes route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'rails/welcome#index'
+    namespace :api do
+    namespace :v1 do
+      resources :random_recipes, only: [:index]
+    end 
+  end 
 end
