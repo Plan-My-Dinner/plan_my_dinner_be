39ce21664f4077a0342de85d4ae3fcc276b1f046
Aleisha Mork <aleisha.mork@gmail.com>
2023-08-05 01:02:38 -0600

    Added summary of what the regexp in email format validation is checking

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,10 @@
 class User < ApplicationRecord
-  ### This Regexp checks that an email: 
-  # \A - start of string
-  # (...) - capture group
-  # [...] - list of possible characters (eg: a or b) not a and b
-  # + - occurs one or more times
+  ### The Regexp (ignoring case) checks that an email:
+    ## Begins with at least one none space or "@" symbol character, captured data (local address)
+    ## Has a "@" symbol
+    ## The "@" is followed by at least one alphanumeric or dash character ending in a period, non-captured data (host)
+    ## Ends with a minimum of 2 alpha characters, captured data (domain)
 
-  # ^ - Matches beginning of line or when in [] matches any character except those named
-  # @ - Literally looking for @ symbol
-  # \s - whitespace character
-  # (?:...) - non-captured group (will not be returned in captured data but must be a match) 
-  # - = literally the dash character
-  # a-z - the range of lowercase letters a-z
-  # 0-9 - the range of numbers 0-9
-  # \. - A period
-  # {2,} - occurs 2 or more times
-  # \z - matches end of string
-  # /i - ignore case
-  
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :email, uniqueness: { case_sensitive: false }
   validates :password_digest, presence: true
