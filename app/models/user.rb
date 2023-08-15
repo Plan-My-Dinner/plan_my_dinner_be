a11c2673459746569f6274de5e7256a6e3a1a920
Aleisha Mork <aleisha.mork@gmail.com>
2023-08-15 17:55:34 -0600

    Corrected validations and added more complete testing for password and email format requirments
    Change how email format is tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,5 @@ class User < ApplicationRecord
 
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :email, uniqueness: { case_sensitive: false }
-  validates :password_digest, presence: true
   has_secure_password
 end
