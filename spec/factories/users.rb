9946263eb72130b556453d4340f915398067b5ce
Aleisha Mork <aleisha.mork@gmail.com>
2023-08-15 12:46:43 -0600

    Added Faker data generator to the User factory
    Correct faker data

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { 'MyString' }
-    password_digest { 'MyString' }
+    email { Faker::Internet.email }
+    # Keyword arguments: min_length, max_length, mix_case, special_characters
+    password { Faker::Internet.password }
   end
 end
