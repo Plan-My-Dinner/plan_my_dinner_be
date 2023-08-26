6cf10bb968cd69435d2f4a2cd53628b14ff323a1
Ian Ross <ianrossdrums@gmail.com>
2023-08-26 11:46:46 -0600

    Create saved and scheduled recipie tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_21_213350) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_191958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "saved_recipies", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "api_recipie_id", null: false
+    t.boolean "favorited", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_saved_recipies_on_user_id"
+  end
+
+  create_table "scheduled_recipies", force: :cascade do |t|
+    t.bigint "saved_recipie_id", null: false
+    t.datetime "scheduled_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["saved_recipie_id"], name: "index_scheduled_recipies_on_saved_recipie_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_213350) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "saved_recipies", "users"
+  add_foreign_key "scheduled_recipies", "saved_recipies", column: "saved_recipie_id"
 end
