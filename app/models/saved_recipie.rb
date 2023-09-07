31bf3e98b160c878a1b629ec34d1768cc6e42c11
Ian Ross <ianrossdrums@gmail.com>
2023-09-07 11:58:33 -0600

    Update validations

--- app/models/saved_recipie.rb
+++ app/models/saved_recipie.rb
@@ -2,7 +2,7 @@ class SavedRecipie < ApplicationRecord
   ## VALIDATIONS
     validates :user_id, presence: true
     validates :user, presence: true
-    validates :api_recipie_id, numaricality: {only_integer: true}
+    validates :api_recipie_id, numericality: { only_integer: true }
     validates :favorited, inclusion: [true, false]
 
   ## RELATIONSHIPS
