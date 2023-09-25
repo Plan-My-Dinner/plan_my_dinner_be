ff15392b46f84d9d24e900b589655f11aa0b8477
Ian Ross <ianrossdrums@gmail.com>
2023-09-24 18:15:41 -0600

    Update 3rd party API call

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       ##Recipe endpoints
-      get "/recipe/:id", to: "recipe#show" #get recipe information
+      get "/recipe/:id", to: "saved_recipe#show" #get recipe information
     end
   end
 end
